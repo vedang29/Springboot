a1547260a833cc3ed0ac5b693aac6f3c3bc7b25a
vedang29 <vedang294@gmail.com>
2025-07-03 09:29:46 +0530

    REST CRUD APIs - (Rest Controller, REST POJO, Rest Path Variables, REST Exception Handling)

---
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/entity/Student.java
@@ -0,0 +1,31 @@
+package com.luv2code.demo.entity;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+
+    public Student(){
+
+    }
+
+    public Student(String lastName, String firstName) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
